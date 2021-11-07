f794cce3f04b716f4712b88e6de0bf81c2f12fe6
Hao-Xin Wang <wanghx18@mails.tsinghua.edu.cn>
2021-11-07 22:09:42 +0800

    add include tdvp to gqmps.h

--- include/gqmps2/gqmps2.h
+++ include/gqmps2/gqmps2.h
@@ -33,6 +33,7 @@
 #include "gqmps2/algorithm/lanczos_solver.h"                        // LanczosParams
 #include "gqmps2/algorithm/vmps/two_site_update_finite_vmps.h"      // TwoSiteFiniteVMPS, SweepParams
 #include "gqmps2/algorithm/vmps/single_site_update_finite_vmps.h"   // SingleSiteFiniteVMPS
+#include "gqmps2/algorithm/tdvp/two_site_update_finite_tdvp.h"      // TwoSiteFiniteTDVP
 // MPI Algorithms
 #include "gqmps2/algo_mpi/vmps/two_site_update_finite_vmps_mpi.h"   //TwoSiteFiniteVMPS
 // Mock Algorithms
